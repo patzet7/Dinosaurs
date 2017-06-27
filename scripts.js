960392a4492cb8da173dc0e5fe44a19431d18374
patrysia <patrycja@example.com>
2017-06-27 09:37:28 +0200

    Added files: index.html, scripts.js and style.css

---
+++ scripts.js
@@ -0,0 +1,21 @@
+//Script of "Dinosaurs"
+
+var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
+
+var dinosaur = 'triceratops';
+
+var dinosaurUpperCased = dinosaur.toUpperCase();
+		
+		console.log(dinosaurUpperCased);
+
+var textCharsAfter = text.replace('Velociraptor', dinosaurUpperCased);
+		
+		console.log(textCharsAfter);
+
+var halfText = textCharsAfter.substr(0, textCharsAfter.length/2);
+
+		console.log(halfText);
+
+
+
+
